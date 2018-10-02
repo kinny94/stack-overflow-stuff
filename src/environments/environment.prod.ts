bf0d1dae7e5a671c2855bccc65352a2b9a7b363e
Arjun Dass <ad.sam007@gmail.com>
2018-10-02 11:48:47 -0400

    Added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB2LN2xGplsnSJPiQIspNvm98ub3_4-IPo',
+    authDomain: 'info-9d66a.firebaseapp.com',
+    databaseURL: 'https://info-9d66a.firebaseio.com',
+    projectId: 'info-9d66a',
+    storageBucket: 'info-9d66a.appspot.com',
+    messagingSenderId: '861547850859'
+  }
 };
