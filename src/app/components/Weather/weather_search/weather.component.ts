b75991cc19804097d9e8da112ed3143c349094ff
Arjun Dass <ad.sam007@gmail.com>
2018-10-19 10:20:55 -0400

    added component

--- src/app/components/Weather/weather_search/weather.component.ts
+++ src/app/components/Weather/weather_search/weather.component.ts
@@ -1,15 +1,54 @@
+import { Observable } from 'rxjs';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
+  animations: [
+    trigger('changeState', [
+      state('up', style({
+        paddingTop: '2%',
+      })),
+      state('mid', style({
+        paddingTop: '30%',
+      })),
+      transition('mid=>up', [
+        animate('500ms')
+      ]),
+      transition('up=>mid', [
+        animate('500ms')
+      ]),
+    ])
+  ],
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+
+  state = 'mid';
+  isInMid = false;
+  searchResults: Observable<{}>;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onChange( event ) {
+
+    if ( this.state === 'mid' ) {
+      this.state = 'up';
+      this.isInMid = true;
+    }
+
+    if ( event === '' ) {
+      this.state = 'mid';
+    } else {
+      this.getSearchResults( event );
+    }
+
+  }
+
+  getSearchResults( value ) {}
 
 }
